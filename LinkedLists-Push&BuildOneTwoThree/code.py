7d3efa289eb315f7416cb2c2433ecfd5b9e326c2
DmytroMalyk <149358500+DmytroMalyk@users.noreply.github.com>
2024-04-08 15:52:16 +0300

    Create code.py

---
+++ LinkedLists-Push&BuildOneTwoThree/code.py
@@ -0,0 +1,18 @@
+from preloaded import Node
+
+'''
+Node is defined in preloaded like this:
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+'''
+    
+def push(head, data):
+    new = Node(data)
+    new.next = head
+    return new
+
+def build_one_two_three():
+    return push(push(push(None, 3), 2), 1)
